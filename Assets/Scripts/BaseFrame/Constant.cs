6054591ef482fd3f1439145ef69dcd5f8f55980f
kurong00 <kurong00@gmail.com>
2017-07-19 16:45:18 +0800

    常量添加

---
+++ Assets/Scripts/BaseFrame/Constant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FrameWork;
+
+public class Constant :Singleton<Constant>{
+
+    /// <summary>
+    /// 地图相关的常量
+    /// </summary>
+    public float TILE_DOWN_TIME = 0.5f;
+    public int TILE_DOWN_DISTANCE = 8;
+    public float BOTTOM_LENGTH = Mathf.Sqrt(2) * 0.254f;
+}
