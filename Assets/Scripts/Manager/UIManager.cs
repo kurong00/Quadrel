216409f8250802363d8fbf948a55b34c279fe0eb
kurong00 <kurong00@gmail.com>
2017-07-23 10:05:53 +0800

    主界面按钮UI设置

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using FairyGUI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
@@ -9,25 +10,53 @@ public class UIManager : MonoBehaviour {
     /// 单例相关
     /// </summary>
     private Constant UIConstant;
-
     /// <summary>
     /// UI中的控制器相关
     /// </summary>
-    Controller controllerLine;
+    //Controller controllerLine;
     /// <summary>
     /// UI中的动效相关
     /// </summary>
+    Transition transitionLine;
+    Transition transitionButtonNormal;
+    Transition transitionButtonChanllenge;
+    Transition transitionButtonExit;
+    /// <summary>
+    /// UI中的按钮相关
+    /// </summary>
+    GButton buttonNormal;
+    GButton buttonChanllenge;
+    GButton buttonExit;
     void Start () {
         UIConstant = Constant.Instance();
         GRoot.inst.SetContentScaleFactor(UIConstant.HEIGHT, UIConstant.WIDE);
         UIPackage.AddPackage("UI/主界面");
         GComponent componentMain = UIPackage.CreateObject("主界面", "主界面").asCom;
         GRoot.inst.AddChild(componentMain);
-        GComponent ComponentLine = componentMain.GetChild("n2").asCom;
-        controllerLine = ComponentLine.GetController("line_c");
+        GComponent ComponentLine = componentMain.GetChild("line").asCom;
+        transitionLine = ComponentLine.GetTransition("line_t");
+        buttonNormal= componentMain.GetChild("button_normal").asButton;
+        buttonChanllenge = componentMain.GetChild("button_chanllenge").asButton;
+        buttonExit = componentMain.GetChild("button_exit").asButton;
+        transitionButtonNormal = componentMain.GetTransition("button_t");
+        transitionButtonChanllenge = componentMain.GetTransition("button_t");
+        transitionButtonExit = componentMain.GetTransition("button_t");
     }
 	
-	void Update () {
-        controllerLine.selectedIndex = 1;
-	}
+	void Update ()
+    {
+        buttonNormal.onClick.Add(LoadNormalScene);
+        buttonExit.onClick.Add(GameExit);
+    }
+
+    void LoadNormalScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void GameExit()
+    {
+        Application.Quit();
+    }
+    
 }
